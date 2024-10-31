4ef507801f45763e5767ef2aa48896e2b03d7be4
Neocipher Technologies <neociphertechnologies @gmail.com>
2024-10-31 14:41:36 +0300

    second

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,26 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths as needed
-    "./public/index.html",
+    "./app/**/*.{js,jsx,ts,tsx}",          // Includes all files within app and its subdirectories
+    "./app/components/**/*.{js,jsx,ts,tsx}", // Specifically for components directory
+    "./public/index.html",                   // Includes any HTML in public
   ],
   theme: {
     extend: {
       colors: {
-        brand: "#0aff9d", // Example custom color
+        brand: "#0aff9d", 
         text: "#ebecf3",
         darkBg: "#080808",
         lightBg: "#232323",
         light: "#B3C8CF",
-        // Add more custom colors here if needed
       },
       fontFamily: {
         manrope: ["Manrope", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
         jost: ["Jost", "sans-serif"],
-        // Add more custom fonts here if needed
       },
     },
   },
   plugins: [],
-  darkMode: "selector", // Change to "media" for OS preference or "class" for manual control
+  darkMode: "class", // For manual dark mode control
 };
